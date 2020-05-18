77bd0f8780a4003c0bac6102856f31362dee3a0e
Gabriele Concli <gabriele.concli@gmail.com>
2020-05-18 12:11:53 +0200

    fetched timezoneDB api. now displaying timezone

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ window.addEventListener('load', () => {
     let locationCity = document.querySelector('.location-city')
     let locationIcon = document.querySelector('.location-icon')
     let temperatureDescBrief = document.querySelector('.temperature-desc-brief')
+    let locationTimezone = document.querySelector('.location-timezone')
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
@@ -13,15 +14,17 @@ window.addEventListener('load', () => {
             
             console.log(position)
 
-            const proxy = 'http://cors-anywhere.herokuapp.com/' // proxy enables localhost: API calls
-            const api = `${proxy}http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=212def19414499d20674d9580e825e59`
+            const proxy = 'http://cors-anywhere.herokuapp.com/' // proxy enables API calls from localhost: 
+            const api = `${proxy}http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=212def19414499d20674d9580e825e59&units=metric` 
+            
+            const timezonedb = `${proxy}http://api.timezonedb.com/v2.1/get-time-zone?key=9OKQ2IFZWG6V&format=json&by=position&lat=${lat}&lng=${lon}`
 
             fetch(api)
             .then(response => {
                 return response.json()
             })
             .then(data => {
-                console.log(data)
+                console.log("API fetch: openWeatherMap", data)
 
                 // const {} = data.weather; // create a 'weather' Const :>: contains Weather Condition (weather.main) and Description (weather.description) + icon: weather.icon
                 // const {} = data.main; // main.temp, main.temp_min main.temp_max, pressure, humidity, feels_like
@@ -32,15 +35,34 @@ window.addEventListener('load', () => {
                 const city = data.name;
 
                 // Set DOM Elements from the API
-                temperatureDegree.textContent = temp
+
+                temperatureDegree.textContent = temp 
+                // v //
+                // &units=metric   : Celsius
+                // &units=imperial : Fahrenheit
                 temperatureDescBrief.textContent = main
                 temperatureDescription.textContent = description
+
                 locationCity.textContent = city
+
                 locationIcon.textContent = icon
                 
 
             
+            });
+
+            fetch(timezonedb)
+            .then(response => {
+                return response.json()
             })
+            .then(data => {
+                console.log("API fetch: timezoneDB", data)
+
+                const zoneName = data.zoneName
+                const zoneAbbr = data.abbreviation
+
+                locationTimezone.textContent = `${zoneName}, ${zoneAbbr}`
+            });
         })
 
     
